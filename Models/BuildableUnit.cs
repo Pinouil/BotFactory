cbe08e5f10264d5876765c319bdea8df80773292
Aurelien <pcadeaurelien@gmail.com>
2018-05-24 16:34:14 +0200

    Add Model Properties

--- Models/BuildableUnit.cs
+++ Models/BuildableUnit.cs
@@ -10,10 +10,21 @@ namespace BotFactory.Models
     public abstract class BuildableUnit
     {
         private double _buildTime = 5000;
+        private string _model = "Missing Model";
         public double BuildTime
         {
             get { return _buildTime; }
             set { _buildTime = value; }
+        }
+
+        public string Model
+        {
+            get
+            {
+                _model = this.GetType().Name;
+                return _model;
+            }
+            set { _model = value; }
         }
 
         public BuildableUnit(double buildTime)
